c5b123bf7518827bedb401d9050d5346eb3595be
rockbell-E <jonathanilllanes@gmail.com>
2025-03-11 20:46:21 -0300

    Add files via upload
    estructura de la pagina

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+const authRouter = require('./routes/auth');
+app.use('/', authRouter);
+
+const adminRouter = require('./routes/admin');
+app.use('/admin', adminRouter);
+
+const empresaRouter = require('./routes/empresa');
+app.use('/empresa', empresaRouter);
+
+app.use((req, res, next) => {
+  res.status(404).render('errors/404', { title: 'Página no encontrada' });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
