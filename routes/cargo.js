c2b5efa8c8208dbdd8f45fad0ce63d13b7a6a46c
rockbell-E <jonathanilllanes@gmail.com>
2025-04-06 21:00:01 -0400

    Se corrigio la creacion del cargo

--- routes/cargo.js
+++ routes/cargo.js
@@ -3,18 +3,14 @@ const express = require('express');
 const router = express.Router();
 const cargoController = require('../controllers/cargoController');
 
-// Ruta para listar cargos
 router.get('/cargos', cargoController.listCargos);
 
-// Rutas para crear un nuevo cargo
 router.get('/cargos/new', cargoController.getNewCargo);
 router.post('/cargos', cargoController.createCargo);
 
-// Rutas para editar un cargo
 router.get('/cargos/:id/edit', cargoController.getEditCargo);
 router.post('/cargos/:id', cargoController.updateCargo);
 
-// Ruta para eliminar un cargo
 router.post('/cargos/:id/delete', cargoController.deleteCargo);
 
 module.exports = router;
