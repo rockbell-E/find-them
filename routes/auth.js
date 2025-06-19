2ede8a3ff9af4d9aa9ef33108a5983fbae4053d3
rockbell-E <jonathanilllanes@gmail.com>
2025-06-19 01:45:30 -0400

    seccion de recuperar contraseña completa

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getLogin, postLogin, logout } = require('../controllers/unifiedAuthController');
+const { getLogin, postLogin, logout, getForgotPassword, postForgotPassword } = require('../controllers/unifiedAuthController');
 
 router.get('/login', getLogin);
 router.post('/login', postLogin);
